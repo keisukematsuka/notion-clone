a8fb624e2635c73caec3378228c237b6aa6e6ab7
keisukematsuka <matsukakeisuke0202@gmail.com>
2024-07-28 13:11:48 +0900

    適切なコミットメッセージ

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://notion-clone-692f82db0b79.herokuapp.com",
   })
 );
 app.use(express.json());
